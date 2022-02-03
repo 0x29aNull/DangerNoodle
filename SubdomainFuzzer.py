ab3b1b42d6c6e030d62be94c56753668ec4a3476
0x29a Null <78053467+0x29aNull@users.noreply.github.com>
2022-02-02 16:36:01 -0800

    Create SubdomainFuzzer.py

---
+++ SubdomainFuzzer.py
@@ -0,0 +1,52 @@
+# Subdomain Fuzzer
+# Name wordlist as 'list.txt' in same directory
+#
+# 0x29a
+
+import io, requests, sys, threading
+from queue import Queue
+
+domain = input('Enter Domain: ')
+
+WaitingRoom = Queue()
+
+def connect(url):
+    try:
+        requests.get(url)
+        return True
+    except requests.ConnectionError:
+        return False
+
+def getUrl():
+    with open('list.txt', 'r') as file:
+        line = file.readline()
+        cnt = 0
+        while line:
+            url = ("https://{}.{}".format(line.strip(),domain))
+            WaitingRoom.put(url)
+            line = file.readline()
+            cnt += 1
+
+def daemon():
+    while not WaitingRoom.empty():
+        url = WaitingRoom.get()
+        if connect(url):
+            print("{} Exists".format(url))
+        else:
+            print("{} Does Not Exist".format(url))
+
+def scan(threads):
+    getUrl()
+    rThreads = []
+
+    for x in range(threads):
+        thread = threading.Thread(target=daemon)
+        rThreads.append(thread)
+
+    for thread in rThreads:
+        thread.start()
+
+    for thread in rThreads:
+        thread.join()
+
+scan(100)
